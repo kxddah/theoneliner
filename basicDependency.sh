3d4e84aa0ee33e2fada5bf8e94a37e3418213282
Hardik Singh <94586114+kxddah@users.noreply.github.com>
2022-10-26 20:10:15 +0530

    Create basicDependency.sh

---
+++ basicDependency.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install moreutils
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+sudo apt-get install -y git
+sudo apt install -y lolcat
+sudo apt install -y fortune
+sudo apt install -y pv
